4f39b6a893670d9e73ef374bf939cb7d1e48c007
vijaicv <vijaicv@gmail.com>
2020-01-09 15:07:04 +0530

    initial commit

---
+++ SurveySamplePage/src/main/java/com/vijaicv/mvcdemo/SurveySamplePageApplication.java
@@ -0,0 +1,13 @@
+package com.vijaicv.mvcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveySamplePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveySamplePageApplication.class, args);
+	}
+
+}
---
+++ SurveySamplePage/src/test/java/com/vijaicv/mvcdemo/SurveySamplePageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaicv.mvcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveySamplePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
